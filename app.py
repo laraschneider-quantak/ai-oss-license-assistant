0cbb9045e5bc79c85fa4e3e7c5c6ef7c42c476ab
Lara <lara@quantak-solutions.com>
2026-06-07 21:31:37 +0200

    Add license detection and persistent vector database

--- app.py
+++ app.py
@@ -18,6 +18,28 @@ collection = chroma_client.get_or_create_collection(
     name="oss_compliance_knowledge"
 )
 
+def detect_license(query):
+    query_lower = query.lower()
+
+    license_map = {
+        "mit": "mit.txt",
+        "bsd": "bsd.txt",
+        "apache": "apache.txt",
+        "gplv2": "gplv2.txt",
+        "gpl v2": "gplv2.txt",
+        "gplv3": "gplv3.txt",
+        "gpl v3": "gplv3.txt",
+        "lgpl": "lgpl.txt",
+        "agpl": "agpl.txt",
+        "mpl": "mpl.txt"
+    }
+
+    for license_name, filename in license_map.items():
+        if license_name in query_lower:
+            return filename
+
+    return None
+
 st.title("OSS Compliance Assistant")
 
 query = st.text_input(
@@ -26,14 +48,26 @@ query = st.text_input(
 
 if st.button("Ask"):
 
-    results = collection.query(
-        query_texts=[query],
-        n_results=5
-    )
+    detected_license_file = detect_license(query)
 
-    documents_for_gpt = "\n\n".join(
-        results["documents"][0]
-    )
+    if detected_license_file:
+        results = collection.get(
+            ids=[detected_license_file]
+        )
+
+        documents_for_gpt = "\n\n".join(
+            results["documents"]
+        )
+
+    else:
+        results = collection.query(
+            query_texts=[query],
+            n_results=5
+        )
+
+        documents_for_gpt = "\n\n".join(
+            results["documents"][0]
+        )
 
     response = client.responses.create(
         model="gpt-5",
@@ -51,5 +85,4 @@ Question:
     )
 
     st.subheader("Answer")
-
     st.write(response.output_text)
